08960ea6efa741bfb94ec120b1fe00e52ad53f04
Rian N Herdian (KangRian) <kangrian14@gmail.com>
2016-08-25 08:10:47 +0700

    1
    1

---
+++ js/cr.js
@@ -0,0 +1,14 @@
+$(document).ready(function()
+{
+var aa=$("#copyright").val();
+if (aa == null) {
+window.location.href = "href","http://www.kangrian.com/";
+};
+$("#copyright").attr("href","http://www.kangrian.com/");
+$("#copyright").text("Kang Rian");
+$("#copyright").attr("title","KangRian.com | Creative Web & Graphic Designer from Bandung, Indonesia.");
+$("#copyright").attr("style","display: inline-block !important");    
+$("#copyright").attr("style","position:relative!important");
+$("#copyright").css("position","relative");
+$("#copyright").css("zIndex","+1000000");
+});
