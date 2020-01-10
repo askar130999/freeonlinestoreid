f1384bbcc0c5186fa5e4ad327fe0f99842d52722
Kang Rian リアンさん <kangrian14@gmail.com>
2020-01-10 14:20:52 +0700

    Update license.js

--- js/license.js
+++ js/license.js
@@ -1,1 +1,1 @@
-var customLSN = ['localhost','w3static.github.io','pacul.kangrian.net']; var redirectURL = 'https://kangrian.net';
+var customLSN = ['localhost','w3static.github.io']; var redirectURL = 'https://kangrian.net';
