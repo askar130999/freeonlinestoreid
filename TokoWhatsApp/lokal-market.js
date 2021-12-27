00973bb91679a0113574ac28e8e368831b3ed675
Kang Rian リアンさん <kangrian14@gmail.com>
2021-12-27 18:49:02 +0700

    Update lokal-market.js

--- TokoWhatsApp/lokal-market.js
+++ TokoWhatsApp/lokal-market.js
@@ -6,7 +6,7 @@
 
     Mengubah / Menghapus file javascript (js) ini dapat mengakibatkan :
 
-    1. Kerusakan / error pada tempalte
+    1. Kerusakan / error pada template
     2. Menghilangkan fungsi update / perbaikan otomatis pada template
     3. Tidak akan mendapatkan support/bantuan dari kreator template
 
@@ -15,7 +15,7 @@
     Salam,
     KangRian.NET ( Web & UI/UX Designer )
 
-    Terakhir Diperbarui : 27/12/2021 @ 18:45
+    Terakhir Diperbarui : 27/12/2021 @ 18:48
 
 ================================================================================================ */
 
